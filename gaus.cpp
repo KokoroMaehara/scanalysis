fbaa23c5e0fd161b5cd90ea8405aa90cba2b37d1
KokoroMaehara <kmaehara@hepl.phys.nagoya-u.ac.jp>
2019-02-22 13:32:34 +0900

    Change open file

--- gaus.cpp
+++ gaus.cpp
@@ -3,7 +3,7 @@
      gStyle->SetOptFit();
      //Style->SetOptFit();
      TCanvas *c1 = new TCanvas("c1", "c1 title", 200, 100, 800, 600); //綺麗に見える設定らしい
-     TFile *_file0 = TFile::Open("keiko_1124.root");                  //TFile::Open("xxx.yyy"):ファイル"xxx.yyy"を開く
+     TFile *_file0 = TFile::Open("keiko_1122_9.root");                //TFile::Open("xxx.yyy"):ファイル"xxx.yyy"を開く
 
      TH1F *h1 = new TH1F("h1", "", 2000, 0, 25);
      v1290->Draw("0.02441*(drift[9]-drift[6])-12 >> h1"); //h(指定範囲の分割数,左端,右端)
@@ -17,5 +17,5 @@
      h1->Fit("f1", "", "", 0, 3);                          //xが0.000000001から10の範囲についてフィット
 
      f1->Draw("same");    //読み込んだデータに重ねて書く
-     c1->Print("03.png"); //xxx.pngの形式で書き出す
+     c1->Print("04.png"); //xxx.pngの形式で書き出す
 }
