1298e2d2cd69ac5196ed51a82982088d32556c86
KokoroMaehara <kmaehara@hepl.phys.nagoya-u.ac.jp>
2019-02-22 13:21:24 +0900

    Change hist title

--- gaus.cpp
+++ gaus.cpp
@@ -5,7 +5,8 @@
      TCanvas *c1 = new TCanvas("c1", "c1 title", 200, 100, 800, 600); //綺麗に見える設定らしい
      TFile *_file0 = TFile::Open("keiko_1124.root");                  //TFile::Open("xxx.yyy"):ファイル"xxx.yyy"を開く
 
-     v1290->Draw("0.02441*(drift[9]-drift[6]) >> h(2000,0.0000001,25)"); //h(指定範囲の分割数,左端,右端)
+     TH1F *h1 = new TH1F("h1", "", 2000, 0.0000001, 25);
+     v1290->Draw("0.02441*(drift[9]-drift[6]) >> h1"); //h(指定範囲の分割数,左端,右端)
      double x;
      TF1 *f1 = new TF1("f1", "[0]*TMath::Gaus(x,[1],[2])", 11, 15); //フィッティングする為の関数の定義。[]で囲んである部分がフィットで変化させるパラメーター。関数を使うときはTMath::expのようにする。TMath::Gaus(Double_t x,Double_t mean,Double_t sigma)
      f1->SetNpx(10000);                                             //フィットした関数をプロットする細かさ
