c4c7700a7bc348269bf3a66c31ffe63fc5033221
KokoroMaehara <kmaehara@hepl.phys.nagoya-u.ac.jp>
2019-02-22 11:36:16 +0900

    Modify hrange

--- gaus.cpp
+++ gaus.cpp
@@ -2,20 +2,19 @@
 {
      gStyle->SetOptFit();
      //Style->SetOptFit();
-     TCanvas*c1 = new TCanvas("c1","c1 title", 200, 100, 800, 600);//綺麗に見える設定らしい
-     TFile *_file0 = TFile::Open("keiko_1122_9.root");//TFile::Open("xxx.yyy"):ファイル"xxx.yyy"を開く
+     TCanvas *c1 = new TCanvas("c1", "c1 title", 200, 100, 800, 600); //綺麗に見える設定らしい
+     TFile *_file0 = TFile::Open("keiko_1122_9.root");                //TFile::Open("xxx.yyy"):ファイル"xxx.yyy"を開く
 
-     v1290 ->Draw("0.025*(drift[11]-drift[13]) >> h(2000,-1,10)");//h(指定範囲の分割数,左端,右端)    
+     v1290->Draw("0.025*(drift[11]-drift[13]) >> h(2000,0.000000001,10)"); //h(指定範囲の分割数,左端,右端)
      double x;
-     TF1 * f1 =new TF1("f1","[0]*TMath::Gaus(x,[1],[2])",0.000000001,10);//フィッティングする為の関数の定義。[]で囲んである部分がフィットで変化させるパラメーター。関数を使うときはTMath::expのようにする。TMath::Gaus(Double_t x,Double_t mean,Double_t sigma)
-     f1->SetNpx(10000);//フィットした関数をプロットする細かさ    
+     TF1 *f1 = new TF1("f1", "[0]*TMath::Gaus(x,[1],[2])", 0.000000001, 10); //フィッティングする為の関数の定義。[]で囲んである部分がフィットで変化させるパラメーター。関数を使うときはTMath::expのようにする。TMath::Gaus(Double_t x,Double_t mean,Double_t sigma)
+     f1->SetNpx(10000);                                                      //フィットした関数をプロットする細かさ
 
-     f1->SetParameters(1,0,30);//パラメータの初期値を決める
+     f1->SetParameters(1, 0, 30); //パラメータの初期値を決める
 
-     f1->SetParNames("constant","mean","sigma");//パラメータの名前を決める
-     h->Fit("f1","","",0.000000001,10);//xが0.000000001から10の範囲についてフィット
-    
-     f1->Draw("same");//読み込んだデータに重ねて書く
-     c1->Print("01.png");//xxx.pngの形式で書き出す
-   
+     f1->SetParNames("constant", "mean", "sigma"); //パラメータの名前を決める
+     h->Fit("f1", "", "", 0.000000001, 10);        //xが0.000000001から10の範囲についてフィット
+
+     f1->Draw("same");    //読み込んだデータに重ねて書く
+     c1->Print("01.png"); //xxx.pngの形式で書き出す
 }
