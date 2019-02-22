2462f117a68b8a7cfea5ddd140c69db3d5fc140b
KokoroMaehara <kmaehara@hepl.phys.nagoya-u.ac.jp>
2019-02-22 11:12:12 +0900

    Bug fix: move line which defines h1

--- tres.py
+++ tres.py
@@ -3,6 +3,7 @@ from ROOT import gStyle,TCanvas,TFile,TF1,TH1F
 gStyle.SetOptFit()
 
 c1 = TCanvas("c1","c1 title", 200, 100, 800, 600) # 綺麗に見える設定らしい
+h1 = TH1F("h1","h1 title",10000,-1,10)
 
 file = TFile("keiko_1122_9.root")
 t1 = file.Get("v1290")
@@ -14,7 +15,6 @@ f1.SetNpx(10000) # フィットした関数をプロットする細かさ
 f1.SetParameters(1,0,30) # パラメータの初期値を決める
 f1.SetParNames("constant","mean","sigma") # パラメータの名前を決める
 
-h1 = TH1F("h1","h1 title",10000,-1,10)
 h1.Fit("f1","","",0.000000001,10) # xが0.000000001から10の範囲についてフィット
 
 f1.Draw("same") # 読み込んだデータに重ねて書く
